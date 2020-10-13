6ff3a6a79156c822b9b91ac60beb2c07dffe3b7b
wb_zhouping_cd <wb_zhouping_cd@keruyun.com>
2020-10-13 22:19:55 +0800

    dswff

---
+++ .my/login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="bg">
+    <h1>登录</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    // props: {
+    //   // url查询参数名称
+    //   urlQueryName: {
+    //     type: String,
+    //     default: 'url'
+    //   },
+    //   // 默认登录成功跳转页面地址
+    //   defaultUrl: {
+    //     type: String,
+    //     default: '/'
+    //   }
+    // },
+    // methods: {
+    //   login({account, password}) {
+    //     // todo: 根据业务实现对接登录接口
+    //     return new Promise((resolve, reject) => {
+    //       setTimeout(() => {
+    //         resolve({
+    //             id: 123,
+    //             token: 'abc'
+    //           }
+    //         )
+    //       }, 300)
+    //     })
+    //   },
+    //   handleSuccess(res) {
+    //     // 在此记录登录信息和跳转
+    //     if (!this.$access) return
+    //     this.$access.login(res)
+    //     this.redirect()
+    //   },
+    //   isSelf(url) {
+    //     const {fullPath, path} = this.$route
+    //     return url === fullPath || url === path || url === window.location.href
+    //   },
+    //   redirect() {
+    //     const redirectUrl = this.$route.query[this.urlQueryName] || this.defaultUrl
+    //     const path = decodeURIComponent(redirectUrl)
+    //     // 如果准备要跳转的地址就是页面本身，不需要处理
+    //     if (this.isSelf(path)) return
+    //     const regex = /^http(s)?:\/\/.+/
+    //     if (regex.test(path)) {
+    //       window.location.href = path
+    //     } else {
+    //       this.$router.push(path)
+    //     }
+    //   }
+    // },
+    // created() {
+    //   // 如果已经是登录状态，跳转到首页或指定页面
+    //   if (this.$access && this.$access.isLogin()) {
+    //     this.redirect()
+    //   }
+    // }
+  }
+</script>
+
---
+++ .my/theme-2.vue
@@ -0,0 +1,50 @@
+<template>
+  <table>
+     <tr>
+      <th width="80">用途</th>
+      <th width="200">变量名</th>
+      <th width="80">默认值</th>
+      <th>效果预览</th>
+    </tr>
+    <tr>
+      <td>宽松</td>
+       <td>$--line-height-large</td>
+       <td>2em</td>
+       <td class="line-height-large"><span v-for="n in 20" :key="n">文字样本</span></td>
+    </tr>
+    <tr>
+      <td>常规</td>
+       <td>$--line-height-normal</td>
+       <td>1.5em</td>
+       <td class="line-height-normal"><span v-for="n in 20" :key="n">文字样本</span></td>
+    </tr>
+    <tr>
+      <td>紧凑</td>
+       <td>$--line-height-small</td>
+       <td>1.3em</td>
+       <td class="line-height-small"><span v-for="n in 20" :key="n">文字样本</span></td>
+    </tr>
+      <tr>
+        <td>无行高</td>
+         <td>$--line-height-none</td>
+         <td>1em</td>
+         <td class="line-height-none"><span v-for="n in 20" :key="n">文字样本</span></td>
+      </tr>
+    </table>
+</template>
+
+<style>
+  .line-height-large {
+     line-height: 2em;
+  }
+  .line-height-normal {
+     line-height: 1.5em;
+  }
+  .line-height-small {
+     line-height: 1.3em;
+  }
+  .line-height-none {
+     line-height: 1em;
+  }
+</style>
+
